030d1d2c189a38cb4d8aba4b7ed40627d4a66558
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-08 23:52:45 +0200

    [025] Exceptions

---
+++ Exceptions/AccidentException.cs
@@ -0,0 +1,12 @@
+﻿namespace Exceptions
+{
+    public class AccidentException : Exception
+    {
+        public string Location { get; set; }
+        public AccidentException(string location, string message) : base(message)
+        {
+            Location = location;
+
+        }
+    }
+}
---
+++ Exceptions/InvalidAddressException.cs
@@ -0,0 +1,10 @@
+﻿namespace Exceptions
+{
+    public class InvalidAddressException : Exception
+    {
+        public InvalidAddressException(string message) : base(message)
+        {
+
+        }
+    }
+}
