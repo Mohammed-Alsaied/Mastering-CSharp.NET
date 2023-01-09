069ca9c2d581cf0af3b7604edacf16da3ace2b6d
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-09 17:23:41 +0200

    [017] Nested Types

---
+++ [017] Nested Types/Program.cs
@@ -0,0 +1,32 @@
+﻿//Nested Type -?Type Defiend Within A class
+
+//Person p = new Person();
+//InternalPerson ip = new InternalPerson();
+
+Employee e1 = new Employee();
+Console.WriteLine(e1.EmployeeInsurance.CompanyName);
+class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Insurance EmployeeInsurance { get; set; }
+    public Employee() =>
+   EmployeeInsurance = new Insurance
+   {
+       PolicyId = -1,
+       CompanyName = "N/A",
+   };
+    public class Insurance
+    {
+        public int PolicyId { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+}
+
+class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
