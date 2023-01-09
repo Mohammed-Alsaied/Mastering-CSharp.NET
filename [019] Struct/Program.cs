27edb9810df76724d70a54bb3c21041c43b1e3ac
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-09 17:27:20 +0200

    [019] Struct

---
+++ [019] Struct/Program.cs
@@ -0,0 +1,62 @@
+﻿
+class Program
+{
+    /*Struct VS Class
+
+->Struct                                    ->Class
+1.Struct key word                           1.class key word
+2.Constructor                               2.Constructor
+3.No Parameter less Constructor             3.Parameter less Constructor
+4.Support Fields                            4.Support Fields
+5.NO Field Intializer                       5.Field Intializer
+6.Support Methods                           6.Support Methods
+7.Support Properties                        7.Support Properties
+8.Support Event                             8.Support Event
+9.indexers                                  9.indexers
+10.Operator Oveloading                      10.Operator Oveloading
+11.No Finalizer (Destructor)                11.Finalizer (Destructor)
+12.Not Support Inheritance                  12.Support Inheritance
+13.Implicitly Inhirit Object Class          13.Implicitly Inhirit Object Class
+14.Not Support Large Data Max 16 Byte       14. Support Large Data
+15.Not Support String                       15.Support String
+16.Value Type Stored in Stack               16.Reference Type Object Instance Stored in Stack And Value Stored In Heap
+17.new() not mandatory                      17.new()  mandatory
+18.Immutable                                18.Mutable
+
+
+
+
+*/
+
+    static void Main(string[] args)
+    {
+
+
+    }
+
+
+
+
+}
+
+
+struct Student
+{
+    //protected int id = 100;
+    //protected void show()
+    //{
+
+    //}
+    //public Student()
+    //{
+
+    //}
+    //public Student(string name)
+    //{
+
+    //}
+
+    //~Student{
+
+    //}
+}
